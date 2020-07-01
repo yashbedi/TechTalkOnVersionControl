62595e65683f9f4bbcc5f1775778993945967730
yash <yash.bedi@paytm.com>
2020-07-01 16:17:17 +0530

    initial commit

---
+++ TestingProjForTechTalk/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TestingProjForTechTalk
+//
+//  Created by Yash Bedi on 01/07/20.
+//  Copyright © 2020 Yash Bedi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
